ee8ab2c8bce40a9484de935984ff3ba33d2a70e5
Chethan S Gowda <yourgithubemail@gmail.com>
2026-06-09 18:19:27 +0530

    add my react first project

---
+++ notes/React/projects/my-frist-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Studentcard from "./Studentcard";
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>My First React App</p>
+      <Studentcard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ notes/React/projects/my-frist-react-app/src/studentcard.jsx
@@ -0,0 +1,10 @@
+function Studentcard(){
+    return(
+        <div>
+            <h1>chethan</h1>
+            <p>ISE</p>
+            </div>
+    );
+}
+
+export default Studentcard;
