c60483138e5d214342d30480575924098a5bfb56
Chethan S Gowda <yourgithubemail@gmail.com>
2026-06-09 22:04:04 +0530

    props using projec

---
+++ notes/React/projects/props/src/App.jsx
@@ -0,0 +1,24 @@
+import Sc from "./Sc";
+function App(){
+  return(
+    <div>
+      <Sc
+      name="chethan"
+      age = "20"
+      />
+      <Sc
+      name="chethan"
+      age = "20"
+      />
+      <Sc
+      name="chethan"
+      age = "20"
+      />
+      <Sc
+      name="chethan"
+      age = "20"
+      />
+    </div>
+  );
+}
+export default App;
---
+++ notes/React/projects/props/src/sc.jsx
@@ -0,0 +1,9 @@
+function Sc(props){
+    return(
+        <div>
+             <h1>NAME: {props.name}</h1>
+             <p> AGE: {props.age}</p>   
+        </div>
+    );
+}
+export default Sc;
